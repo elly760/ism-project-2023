ccf5df9d4b5922a39d49966e3da2f6e210d80393
elly760 <121493150+elly760@users.noreply.github.com>
2023-02-28 10:10:58 +0300

    Update login.php

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Admin | Voting System</title>
+  <title>Welcome to online voting system</title>
  	
 
 <?php include('./header.php'); ?>
