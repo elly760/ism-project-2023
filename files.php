315ab2cc1eea27f54d79bffe2582ccd29e41fb21
ISMGROUP <122868410+ISMGROUP@users.noreply.github.com>
2023-03-07 12:17:25 +0300

    Update files.php

--- files.php
+++ files.php
@@ -2,6 +2,7 @@
 include 'db_connect.php';
 $folder_parent = isset($_GET['fid'])? $_GET['fid'] : 0;
 $folders = $conn->query("SELECT * FROM folders where parent_id = $folder_parent and user_id = '".$_SESSION['login_id']."'  order by name asc");
+$folders = $conn->query("SELECT * FROM folders where parent_id = $folder_parent and user_id = '".$_SESSION['login_id']."'  order by name asc");
 
 
 $files = $conn->query("SELECT * FROM files where folder_id = $folder_parent and user_id = '".$_SESSION['login_id']."'  order by name asc");
