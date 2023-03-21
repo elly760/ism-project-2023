77010707cc7fc8695e6a589944e0fb0804d5d64e
ISAACakol <122935389+ISAACakol@users.noreply.github.com>
2023-03-21 09:49:48 +0300

    Update db_connect.php
    Please check the updated

--- db_connect.php
+++ db_connect.php
@@ -1,4 +1,4 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','voting_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('localhost','root','','voting_db')or die("Could not connect to the mysql".mysqli_error($con));
 // i have addedvthis line of block
